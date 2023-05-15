5502394cb94553074267dd878eb2e8d2e6955d3b
Inseo_Hwang <90232567+Seeooo-0@users.noreply.github.com>
2023-05-15 19:04:08 +0900

    Update camera_calibration.py
    To find camera calibration results!

--- camera_calibration_pose_estimation_chessboard.py
+++ camera_calibration_pose_estimation_chessboard.py
@@ -18,9 +18,9 @@ def select_img_from_video(input_file, board_pattern, select_all=False, wait_msec
             img_select.append(img)
         else:
             # Show the image
-            display = img.copy()
-            cv.putText(display, f'NSelect: {len(img_select)}', (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
-            cv.imshow('Camera Calibration', display)
+            display_img = img.copy()
+            cv.putText(display_img, f'NSelect: {len(img_select)}', (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
+            cv.imshow('Camera Calibration', display_img)
 
             # Process the key event
             key = cv.waitKey(wait_msec)
@@ -28,8 +28,8 @@ def select_img_from_video(input_file, board_pattern, select_all=False, wait_msec
                 break
             elif key == ord(' '):          # 'Space' key: Pause and show corners
                 complete, pts = cv.findChessboardCorners(img, board_pattern)
-                cv.drawChessboardCorners(display, board_pattern, pts, complete)
-                cv.imshow('Camera Calibration', display)
+                cv.drawChessboardCorners(display_img, board_pattern, pts, complete)
+                cv.imshow('Camera Calibration', display_img)
                 key = cv.waitKey()
                 if key == 27: # ESC
                     break
@@ -59,8 +59,8 @@ def calib_camera_from_chessboard(images, board_pattern, board_cellsize, K=None, 
 
 
 if __name__ == '__main__':
-    input_file = 'chess.mp4'
-    board_pattern = (10, 7)
+    input_file = './chessboard_video.mp4'
+    board_pattern = (8, 6)
     board_cellsize = 0.025
 
     img_select = select_img_from_video(input_file, board_pattern)
@@ -73,63 +73,3 @@ if __name__ == '__main__':
     print(f'* RMS error = {rms}')
     print(f'* Camera matrix (K) = \n{K}')
     print(f'* Distortion coefficient (k1, k2, p1, p2, k3, ...) = {dist_coeff.flatten()}')
-    
-    
-
-# The given video and calibration data
-input_file = 'chess.mp4'
-K = np.array([[432.7390364738057, 0, 476.0614994349778],
-              [0, 431.2395555913084, 288.7602152621297],
-              [0, 0, 1]])
-dist_coeff = np.array([-0.2852754904152874, 0.1016466459919075, -0.0004420196146339175, 0.0001149909868437517, -0.01803978785585194])
-board_pattern = (10, 7)
-board_cellsize = 0.025
-board_criteria = cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_NORMALIZE_IMAGE + cv.CALIB_CB_FAST_CHECK
-
-# Open a video
-video = cv.VideoCapture(input_file)
-assert video.isOpened(), 'Cannot read the given input, ' + input_file
-
-# Prepare a 3D box for simple AR
-box_lower = board_cellsize * np.array([[4, 2,  0], [5, 2,  0], [5, 4,  0], [4, 4,  0]])
-box_upper = board_cellsize * np.array([[4, 2, -1], [5, 2, -1], [5, 4, -1], [4, 4, -1]])
-
-# Prepare 3D points on a chessboard
-obj_points = board_cellsize * np.array([[c, r, 0] for r in range(board_pattern[1]) for c in range(board_pattern[0])])
-
-# Run pose estimation
-while True:
-    # Read an image from the video
-    valid, img = video.read()
-    if not valid:
-        break
-
-    # Estimate the camera pose
-    complete, img_points = cv.findChessboardCorners(img, board_pattern, board_criteria)
-    if complete:
-        ret, rvec, tvec = cv.solvePnP(obj_points, img_points, K, dist_coeff)
-
-        # Draw the box on the image
-        line_lower, _ = cv.projectPoints(box_lower, rvec, tvec, K, dist_coeff)
-        line_upper, _ = cv.projectPoints(box_upper, rvec, tvec, K, dist_coeff)
-        cv.polylines(img, [np.int32(line_lower)], True, (255, 0, 0), 2)
-        cv.polylines(img, [np.int32(line_upper)], True, (0, 0, 255), 2)
-        for b, t in zip(line_lower, line_upper):
-            cv.line(img, np.int32(b.flatten()), np.int32(t.flatten()), (0, 255, 0), 2)
-
-        # Print the camera position
-        R, _ = cv.Rodrigues(rvec) # Alternative) scipy.spatial.transform.Rotation
-        p = (-R.T @ tvec).flatten()
-        info = f'XYZ: [{p[0]:.3f} {p[1]:.3f} {p[2]:.3f}]'
-        cv.putText(img, info, (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
-
-    # Show the image and process the key event
-    cv.imshow('Pose Estimation (Chessboard)', img)
-    key = cv.waitKey(10)
-    if key == ord(' '):
-        key = cv.waitKey()
-    if key == 27: # ESC
-        break
-
-video.release()
-cv.destroyAllWindows()
